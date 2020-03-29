3b93fea48b4e96edd7677660c71ced653fd08bf6
John Speck <speckworks@gmail.com>
2020-03-29 00:21:15 -0400

    added weather widget

--- src/weather.js
+++ src/weather.js
@@ -5,7 +5,24 @@ const Weather = () => {
 
     return (
         <div>
-          <p>This is Where the Weather will be displayed</p>
+          <div style={{width: "100px"}}>
+          <iframe style={{display: "block", textAlign: "center", transform: "translate(550px, -170px)"}} 
+                  src="https://cdnres.willyweather.com/widget/loadView.html?id=121344" 
+                  width="799" 
+                  height="82" 
+                  frameborder="0" 
+                  scrolling="no">
+
+          </iframe>
+          <a style={{
+                    margin: "-20px", 
+                    zIndex: "1", 
+                    display: "block", 
+                    textIndent: "-9999em", 
+                    position: "relative",
+                    height: "20px"}} 
+              href="https://www.willyweather.com/ny/new-york-county/new-york.html" 
+              rel="nofollow">New York Weather</a></div>
           <button onClick={() => setCount(count + 1)}>
             Click me
           </button>
